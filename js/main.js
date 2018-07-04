ef216296a9f96a0c2a9417db231fde8c1bf5c71c
Rustam <ru7ar7@gmail.com>
2018-07-05 01:35:52 +0600

    index.html: добавлена анимация закрытия формы.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // Скрипты для проекта "Sedona"
 //
 
+var form = document.querySelector(".find-hotel form");
+
 // Объект и обработчик нажатия на кнопку 'показать форму'
 var btnShow = document.querySelector(".btn-header");
 btnShow.addEventListener("click", function (evt) {
